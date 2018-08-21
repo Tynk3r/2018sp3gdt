8f5bb35fb18c1d291eae1950d4d834b9ebd23a64
coen <valencoen222@gmail.com>
2018-08-21 17:08:49 +0800

    additions for animating boss

---
+++ Base/Source/Rigs/JointInfo.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Vector3.h"
+#include <string>
+
+class CJointInfo
+{
+public:
+	
+private:
+	std::string jointName;
+
+	Vector3 posOffset;
+};
+
---
+++ Base/Source/Rigs/RigInfo.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vector3.h"
+#include <string>
+#include <map>
+
+class CRigInfo
+{
+public:
+	enum RIG_TYPE
+	{
+		RIG_NONE,
+		RIG_BOSS,
+	};
+private:
+	
+};
+
